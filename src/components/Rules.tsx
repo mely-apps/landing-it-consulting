273c5bc3bbd05aa0d9ad7682e63c3b1191636985
dung204 <acezombiev4@gmail.com>
2024-09-21 13:42:35 +0700

    responsive: rules

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -10,13 +10,15 @@ const Rules = () => {
       className='container mt-44 rounded-lg border-2 border-[#868686] bg-[#ccc]/10 px-20 py-10 shadow-2xl backdrop-blur-sm'
       id={SECTION_IDS.RULES}
     >
-      <div className='grid grid-cols-4'>
-        <GradualSpacing
-          text='Rules'
-          className='col-span-1 text-2xl font-extrabold uppercase !text-primary'
-        />
-        <div className='col-span-3'>
-          <p className='text-base italic'>
+      <div className='grid grid-cols-4 gap-y-8'>
+        <div className='col-span-4 flex justify-center lg:col-span-1'>
+          <GradualSpacing
+            text='Rules'
+            className='text-2xl font-extrabold uppercase !text-primary'
+          />
+        </div>
+        <div className='col-span-4 lg:col-span-3'>
+          <p className='text-center text-base italic lg:text-left'>
             &quot;Teams select a case study from the challenge provided by the
             organizers, then analyze and design ideas and solutions to address
             the identified issues. Awards will be given to ideas that are
