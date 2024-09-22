ba385cb1bba4df2c1826f6843088c2ab57c7619d
dung204 <acezombiev4@gmail.com>
2024-09-22 19:21:31 +0700

    fix: time counter width must equal to registration form width

--- src/components/TimeCounter.tsx
+++ src/components/TimeCounter.tsx
@@ -16,44 +16,46 @@ const TimeCounter = () => {
   return (
     isClient && (
       <div className='container my-40' suppressHydrationWarning>
-        <h2 className='text-center text-2xl font-extrabold text-primary'>
-          TIME LEFT
-        </h2>
         <Countdown
           date={date}
           renderer={(time) => (
-            <div className='mt-16 flex h-64 w-full flex-wrap items-center justify-around rounded-3xl border border-white bg-white/10 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] sm:justify-center sm:gap-x-20'>
-              <div>
-                <p className='neon-text text-center text-2xl font-extrabold drop-shadow-2xl sm:text-3xl'>
-                  {time.days >= 10 ? time.days : `0${time.days}`}
-                </p>
-                <p className='text-center text-base font-semibold uppercase text-white sm:text-lg'>
-                  Days
-                </p>
-              </div>
-              <div>
-                <p className='neon-text text-center text-2xl font-extrabold drop-shadow-2xl sm:text-3xl'>
-                  {time.hours >= 10 ? time.hours : `0${time.hours}`}
-                </p>
-                <p className='text-center text-base font-semibold uppercase text-white sm:text-lg'>
-                  hours
-                </p>
-              </div>
-              <div>
-                <p className='neon-text text-center text-2xl font-extrabold drop-shadow-2xl sm:text-3xl'>
-                  {time.minutes >= 10 ? time.minutes : `0${time.minutes}`}
-                </p>
-                <p className='text-center text-base font-semibold uppercase text-white sm:text-lg'>
-                  min
-                </p>
-              </div>
-              <div>
-                <p className='neon-text text-center text-2xl font-extrabold drop-shadow-2xl sm:text-3xl'>
-                  {time.seconds >= 10 ? time.seconds : `0${time.seconds}`}
-                </p>
-                <p className='text-center text-base font-semibold uppercase text-white sm:text-lg'>
-                  sec
-                </p>
+            <div className='mx-auto flex h-60 w-full flex-col items-center justify-center gap-y-6 rounded-3xl border border-white bg-white/10 sm:w-3/4 lg:h-64'>
+              <h2 className='text-center text-2xl font-extrabold text-primary'>
+                TIME LEFT
+              </h2>
+              <div className='flex w-full flex-wrap items-center justify-around rounded-3xl [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] md:justify-center md:gap-x-20'>
+                <div>
+                  <p className='neon-text text-center text-3xl font-extrabold drop-shadow-2xl'>
+                    {time.days >= 10 ? time.days : `0${time.days}`}
+                  </p>
+                  <p className='text-center text-base font-semibold uppercase text-white sm:text-lg'>
+                    Days
+                  </p>
+                </div>
+                <div>
+                  <p className='neon-text text-center text-3xl font-extrabold drop-shadow-2xl'>
+                    {time.hours >= 10 ? time.hours : `0${time.hours}`}
+                  </p>
+                  <p className='text-center text-base font-semibold uppercase text-white sm:text-lg'>
+                    hours
+                  </p>
+                </div>
+                <div>
+                  <p className='neon-text text-center text-3xl font-extrabold drop-shadow-2xl'>
+                    {time.minutes >= 10 ? time.minutes : `0${time.minutes}`}
+                  </p>
+                  <p className='text-center text-base font-semibold uppercase text-white sm:text-lg'>
+                    min
+                  </p>
+                </div>
+                <div>
+                  <p className='neon-text text-center text-3xl font-extrabold drop-shadow-2xl'>
+                    {time.seconds >= 10 ? time.seconds : `0${time.seconds}`}
+                  </p>
+                  <p className='text-center text-base font-semibold uppercase text-white sm:text-lg'>
+                    sec
+                  </p>
+                </div>
               </div>
             </div>
           )}
