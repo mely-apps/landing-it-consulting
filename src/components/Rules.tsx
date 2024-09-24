6ff309edf442dbb656a0336674661b8889949f58
dung204 <acezombiev4@gmail.com>
2024-09-24 17:10:06 +0700

    i18n: rules

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -3,8 +3,11 @@ import GradualSpacing from './ui/gradual-spacing';
 import { SECTION_IDS } from '@/constants';
 import RulesMobile from '@/assets/RulesMobile';
 import RulesDesktop from '@/assets/RulesDesktop';
+import { useTranslations } from 'next-intl';
 
 const Rules = () => {
+  const t = useTranslations('HomePage');
+
   return (
     <div
       className='card-gradient-border container mt-44 w-11/12 rounded-lg py-3 shadow-2xl backdrop-blur-sm sm:px-10 md:px-20 md:py-8 lg:py-10'
@@ -13,16 +16,13 @@ const Rules = () => {
       <div className='grid grid-cols-4 gap-y-4'>
         <div className='col-span-4 flex max-lg:justify-center lg:col-span-1'>
           <GradualSpacing
-            text='Rules'
+            text={t('rules.title')}
             className='text-2xl font-extrabold uppercase !text-primary'
           />
         </div>
         <div className='col-span-4 lg:col-span-3'>
           <p className='text-justify text-base italic'>
-            &quot;Teams select a case study from the challenge provided by the
-            organizers, then analyze and design ideas and solutions to address
-            the identified issues. Awards will be given to ideas that are
-            feasible, creative, and exemplify strong teamwork.&quot;
+            &quot;{t('rules.description')}&quot;
           </p>
         </div>
       </div>
