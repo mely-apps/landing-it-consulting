288a7f8727422c7ab0a4c647de70b596ac972d19
dung204 <acezombiev4@gmail.com>
2024-09-24 22:13:54 +0700

    i18n: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 import { FaFacebookF, FaLinkedinIn, FaTiktok, FaYoutube } from 'react-icons/fa';
 import CodeSandBox from './icons/CodeSandBox';
 import MailContact from './icons/MailContact';
+import { useTranslations } from 'next-intl';
 
 const Footer = () => {
+  const t = useTranslations('HomePage');
+
   return (
     <div className='mt-40 bg-[#023C38] py-10'>
       <div className='container grid grid-cols-2 gap-x-4 gap-y-12 md:grid-cols-4'>
@@ -12,22 +15,22 @@ const Footer = () => {
         </div>
         <div>
           <p className='inline-block border-b-2 border-[#419D98] py-1 font-bold'>
-            Đơn vị tổ chức
+            {t('header.organizers')}
           </p>
           <ul className='mt-2 flex flex-col gap-2'>
             <li className='flex gap-x-3'>
               <CodeSandBox />
-              <p>Code MeLy</p>
+              <p>{t('names.codeMelyName')}</p>
             </li>
             <li className='flex gap-x-3'>
               <CodeSandBox />
-              <p>Netcompany</p>
+              <p>{t('names.netcompanyName')}</p>
             </li>
           </ul>
         </div>
         <div>
           <p className='inline-block border-b-2 border-[#419D98] py-1 font-bold'>
-            Liên Hệ
+            {t('footer.contact')}
           </p>
           <ul className='mt-2 flex flex-col gap-2'>
             <li className='flex gap-x-3'>
@@ -40,7 +43,7 @@ const Footer = () => {
         </div>
         <div className='max-md:col-span-2 max-md:flex-col max-md:justify-center'>
           <p className='inline-block border-b-2 border-[#419D98] py-1 font-bold'>
-            Theo dõi chúng tôi tại
+            {t('footer.followUs')}
           </p>
           <div className='mt-2 flex gap-x-3'>
             <div className='flex h-10 w-10 items-center justify-center rounded-lg bg-[#FFFFFF] text-black'>
