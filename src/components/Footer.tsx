7ddcf93d1d0aae181a68fa91f69f1b071b53c871
Storm X <62681489+StormX02@users.noreply.github.com>
2024-09-29 18:23:58 +0000

    Refactor Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,70 +11,91 @@ const Footer = () => {
     <div className='mt-40 bg-[#023C38] py-10'>
       <div className='container grid grid-cols-2 gap-x-4 gap-y-6 md:grid-cols-4'>
         <div className='text-lg font-extrabold max-md:col-span-2'>
-          <p className='max-md:text-center'>IT CONSULTANT</p>
-          <p className='text-primary max-md:text-center'>CHALLENGE</p>
+          <p>IT CONSULTANT</p>
+          <p className='text-primary'>CHALLENGE</p>
         </div>
-        <div>
+        {/* <div>
           <p className='inline-block border-b-2 border-[#419D98] py-1 font-bold'>
             {t('header.organizers')}
           </p>
-          <ul className='mt-2 flex flex-col gap-2'>
-            <li className='flex gap-x-2'>
-              <CodeSandBox className='h-5 w-5' />
-              <p>Code MeLy</p>
-            </li>
-            <li className='flex gap-x-2'>
-              <CodeSandBox className='h-5 w-5' />
-              <p>Netcompany</p>
-            </li>
-          </ul>
-        </div>
+            <ul className='mt-2'>
+              
+              <Facebook />
+              <Link
+                href='https://www.facebook.com/code.mely'
+                className='flex items-center gap-2 transition-all hover:text-primary'
+                target='_blank'
+              >
+                Code MeLy
+              </Link>
+              <Facebook />
+              <Link
+                href='https://www.facebook.com/netcompanyvietnam'
+                className='flex items-center gap-2 transition-all hover:text-primary'
+                target='_blank'
+              >
+                Netcompany Vietnam
+              </Link>
+            </ul>
+        </div> */}
         <div>
           <p className='inline-block border-b-2 border-[#419D98] py-1 font-bold'>
             {t('footer.contact')}
           </p>
           <ul className='mt-2 flex flex-col gap-2'>
             <li className='flex gap-x-2'>
-              <MailContact className='h-6 w-6' />
+              <Facebook />
+              <Link
+                href='https://www.facebook.com/code.mely'
+                className='inline-block flex-1 break-all text-white transition-all hover:text-primary'
+                target='_blank'
+              >
+                Code MeLy
+              </Link>
+            </li>
+            <li className='flex gap-x-2'>
+              <Facebook />
+              <Link
+                href='https://www.facebook.com/netcompanyvietnam'
+                className='inline-block flex-1 break-all text-white transition-all hover:text-primary'
+                target='_blank'
+              >
+                Netcompany Vietnam
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='inline-block border-b-2 border-[#419D98] py-1 font-bold'>
+            {t('footer.contact')}
+          </p>
+          <ul className='mt-2 flex flex-col gap-2'>
+            <li className='flex gap-x-2'>
+              <MailContact className='h-6 w-6 flex-1' />
               <Link
                 href='mailto:contact@codemely.dev'
-                className='inline-block break-all text-white transition-all hover:text-primary'
+                className='inline-block flex-1 break-all text-white transition-all hover:text-primary'
               >
                 contact@codemely.dev
               </Link>
             </li>
             <li className='flex gap-x-2'>
-              <MailContact className='h-6 w-6' />
+              <MailContact className='h-6 w-6 flex-1' />
               <Link
                 href='mailto:recruitment.vn@netcompany.com'
-                className='inline-block break-all text-white transition-all hover:text-primary'
+                className='inline-block flex-1 break-all text-white transition-all hover:text-primary'
               >
                 recruitment.vn@netcompany.com
               </Link>
             </li>
           </ul>
         </div>
-        <div className='max-md:col-span-2 max-md:flex max-md:flex-col max-md:items-center'>
+        <div className='max-md:col-span-2 max-md:flex-col'>
           <p className='inline-block border-b-2 border-[#419D98] py-1 font-bold'>
             {t('footer.followUs')}
           </p>
           <div className='mt-2 flex flex-col gap-2'>
-            <Link
-              href='https://www.facebook.com/code.mely'
-              className='flex items-center gap-2 transition-all hover:text-primary'
-              target='_blank'
-            >
-              <Facebook />
-              Code MeLy
-            </Link>
-            <Link
-              href='https://www.facebook.com/netcompanyvietnam'
-              className='flex items-center gap-2 transition-all hover:text-primary'
-              target='_blank'
-            >
-              <Facebook />
-              Netcompany Vietnam
-            </Link>
             <Link
               href='https://www.codemely.dev/'
               className='flex items-center gap-2 transition-all hover:text-primary'
