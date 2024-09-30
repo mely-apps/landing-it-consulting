0c56ca2f0de2498e28ed08106edac647d0599937
Storm X <62681489+StormX02@users.noreply.github.com>
2024-09-30 14:45:44 +0000

    Refactor Hero component to add background gradient and blur fade animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { IoLocationSharp } from 'react-icons/io5';
 import BlurFade from './ui/blur-fade';
 import { FlipWords } from './ui/flip-words';
+import { BackgroundGradient } from './ui/background-gradient';
 
 const Hero = () => {
   const t = useTranslations('HomePage');
@@ -21,7 +22,7 @@ const Hero = () => {
         className='hero-section relative grid grid-cols-1 items-center px-0 md:container max-[1024px]:bg-none sm:m-0 sm:w-full sm:gap-y-10 sm:px-8 sm:py-10 md:mx-auto md:w-11/12 lg:grid-cols-2'
         id={SECTION_IDS.HOME}
       >
-        <div className='order-2 text-center sm:order-1 lg:text-left'>
+        <div className='order-2 text-center sm:order-1 lg:ml-[50px] lg:text-left'>
           <h1 className='hidden text-center font-montserrat text-3xl font-extrabold leading-[1.2] sm:block md:mt-0 lg:text-left'>
             IT CONSULTANT
             <br />
@@ -49,7 +50,7 @@ const Hero = () => {
         </div>
 
         <div className='order-1 flex h-full items-center justify-center sm:order-2 lg:mt-0'>
-          <div className='bg-poster max-sm:hidden sm:h-[500px] sm:w-[360px] lg:mt-[60px] lg:h-[calc(100%_-_8vw_-_66px)] lg:w-[26vw] xl:h-[580px] xl:w-[378px]'>
+          {/* <div className='bg-poster max-sm:hidden sm:h-[500px] sm:w-[360px] lg:mt-[60px] lg:h-[calc(100%_-_8vw_-_66px)] lg:w-[26vw] xl:h-[580px] xl:w-[378px]'>
             <div className='fade-in-right relative h-full w-full overflow-hidden rounded-3xl'>
               <Image
                 src='/cover.png'
@@ -60,7 +61,24 @@ const Hero = () => {
                 fill
               />
             </div>
-          </div>
+          </div> */}
+          <BlurFade delay={1} duration={0.5} className='mt-20 max-sm:hidden'>
+            <BackgroundGradient
+              className='h-full w-full'
+              containerClassName='w-[300px] h-[421px] sm:h-[570px] sm:w-[406px]'
+            >
+              <div className='relative h-full w-full overflow-hidden rounded-3xl'>
+                <Image
+                  src='/cover.png'
+                  alt='landing-it-consulting'
+                  sizes='auto'
+                  className='object-cover'
+                  priority
+                  fill
+                />
+              </div>
+            </BackgroundGradient>
+          </BlurFade>
 
           <div className='relative h-[600px] w-full overflow-hidden p-0 sm:hidden'>
             <Image
