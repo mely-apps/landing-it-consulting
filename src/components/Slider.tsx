0b910a85d9276251dae17478513fb094d6cbcb92
Nghia Tran <emladevops@protonmail.com>
2024-11-08 21:22:00 +0700

    🚨 Fix linting issues

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -31,10 +31,10 @@ const Slider = () => {
     if (timerRef.current) clearInterval(timerRef.current);
     timerRef.current = setInterval(() => {
       if (!isHovering) {
-        paginate(1)
+        paginate(1);
       }
     }, 2000);
-  }
+  };
 
   useEffect(() => {
     startAutoScroll();
@@ -68,7 +68,7 @@ const Slider = () => {
     <>
       <div className='relative mx-auto w-full max-w-3xl pt-10'>
         <div
-          className='relative h-[500px] max-sm:h-[300px] w-full overflow-hidden rounded-lg'
+          className='relative h-[500px] w-full overflow-hidden rounded-lg max-sm:h-[300px]'
           onMouseEnter={() => setIsHovering(true)}
           onMouseLeave={() => setIsHovering(false)}
         >
@@ -101,7 +101,7 @@ const Slider = () => {
         <Button
           variant='default'
           size='icon'
-          className='absolute left-2 top-1/2 z-10 -translate-y-1/2 hover:opacity-80 transition-all'
+          className='absolute left-2 top-1/2 z-10 -translate-y-1/2 transition-all hover:opacity-80'
           onClick={() => paginate(-1)}
           aria-label='Previous image'
         >
@@ -110,7 +110,7 @@ const Slider = () => {
         <Button
           variant='default'
           size='icon'
-          className='absolute right-2 top-1/2 z-10 -translate-y-1/2 hover:opacity-80 transition-all'
+          className='absolute right-2 top-1/2 z-10 -translate-y-1/2 transition-all hover:opacity-80'
           onClick={() => paginate(1)}
           aria-label='Next image'
         >
